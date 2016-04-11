cbee2eebbb125e099b7eb8f4e62fc24a8ceb3714
Maurits van der Schee <maurits@vdschee.nl>
2016-04-11 21:01:18 +0200

    Added x-clock example from x-tag

---
+++ x-clock/x-clock.js
@@ -0,0 +1,26 @@
+window.components['x-clock'] = function (element, template) {
+	var self = this;
+	var state = {};
+	self.render = function(data) {
+		var now = new Date();
+		hour = "" + now.getHours(); if (hour.length == 1) { hour = "0" + hour; }
+	  minute = "" + now.getMinutes(); if (minute.length == 1) { minute = "0" + minute; }
+	  second = "" + now.getSeconds(); if (second.length == 1) { second = "0" + second; }
+	  state.time = hour + ":" + minute + ":" + second;
+		element.html(Mustache.to_html(template,state));
+	};
+	self.start = function() {
+		self.render();
+		self.interval = setInterval(self.render,1000);
+	};
+	self.stop = function() {
+		clearInterval(self.interval);
+		self.interval = null;
+	};
+	self.startStop = function() {
+		if (self.interval) self.stop();
+		else self.start();
+	};
+	element.on('click','b',self.startStop);
+	self.start();
+};
