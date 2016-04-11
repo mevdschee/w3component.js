fb5cbdbc458b0f84d895a0f56aab10be089444d9
Maurits van der Schee <maurits@vdschee.nl>
2016-04-12 01:48:24 +0200

    Support recursion

--- w3component.js
+++ w3component.js
@@ -1,22 +1,33 @@
 var w3component = $(function(){
 	var self = this;
 	self.components = {};
+	var templates = {};
+	var render = function(path,name){
+		$(['div.w3component[w3component="'+path+'"]']).each(function(){
+			$(this).attr('rendered',1);
+			new self.components[name]($(this),templates[name]);
+		});
+	}
 	var handleComponent = function(){
 		var path = $(this).attr('w3component');
+		if ($(this).attr('rendered')) return;
 		var name = path.split('/').pop();
-		if (self.components[name]) return;
-		self.components[name] = true;
+		if (self.components[name]===null) return;
+		if (self.components[name]!==undefined) return render(path,name);
+		self.components[name] = null;
 		$.ajax({dataType:'text', url: path+'.css',success:function(styles){
 			$('<style>').appendTo('body').text(styles);
 		}});
 		$.ajax({dataType:'text', url: path+'.html',success:function(template){
+			templates[name] = template;
 			$('<script>').attr('src',path+'.js').appendTo('body').on('load',function(){
-				$(['div.w3component[w3component="'+path+'"]']).each(function(){
-					self.components[name]($(this),template);
-				});
+				render(path,name);
 			});
 		}});
-	};
-	$('div.w3component').each(handleComponent);
+	}
+	self.rescan = function(element){
+		element.find('div.w3component').each(handleComponent);
+	}
+	self.rescan($(document));
 	w3component = self;
 });
